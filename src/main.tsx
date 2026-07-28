a016a051b2928c44533cdf72f35aa17afd2a31c2
Maciej Czarnecki <maciejkaczarnecka@gmail.com>
2026-07-28 23:18:03 +0200

    v1.0.15

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // ⬅️ ZMIEŃ NA HashRouter
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import App from "./App";
@@ -11,8 +11,8 @@ import "./styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>  {/* ⬅️ ZMIEŃ NA HashRouter */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
